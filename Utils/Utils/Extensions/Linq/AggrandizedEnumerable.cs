ecfbd9d54408958eea3543b448b71ec6c2a70225
Lokf <wistrom.fredrik@gmail.com>
2017-10-16 21:28:44 +0200

    Some work to begin with...

---
+++ Utils/Utils/Extensions/Linq/AggrandizedEnumerable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lokf.Utils.Extensions.Linq
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class AggrandizedEnumerable
+    {
+        /// <summary>
+        /// Executes the given action on each element in the given sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of elements in the given sequence.</typeparam>
+        /// <param name="source">The sequence of elements.</param>
+        /// <param name="action">The action to execute on each element.</param>
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));           
+            if (action == null) throw new ArgumentNullException(nameof(source));            
+
+            foreach (var element in source)
+            {
+                action(element);
+            }
+        }
+    }
+}
---
+++ Utils/Utils/Types/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lokf.Utils.Types
+{
+    public class Unit : IEquatable<Unit>, IComparable<Unit>
+    {
+        public int CompareTo(Unit other) => 0;
+
+        public override bool Equals(object other) => other is Unit;
+
+        public override string ToString() => "()";
+
+        public override int GetHashCode() => 0;
+
+        public bool Equals(Unit other) => true;
+
+        public static bool operator ==(Unit first, Unit second) => true;
+
+        public static bool operator !=(Unit first, Unit second) => false;
+
+        public static Unit Default() => default(Unit);
+    }
+}
